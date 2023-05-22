736306a0e1e3360c7efe586de73b9bda5e9e0cbe
jvarela28 <jvarela@itba.edu.ar>
2023-05-22 20:51:15 -0300

    Update ClasePersonas.py
    Completando clase Personas

--- ClasePersonas.py
+++ ClasePersonas.py
@@ -1,16 +1,65 @@
 class Personas():
     registroPersonas = {}
 
-    def __init__(self, dni, nombre, apellido, fecnac, email,contraseña):
+    def __init__(self, dni, nombre, apellido, fecnac, email, contraseña):
         self.dni = dni
         self.nombre = nombre
         self.apellido = apellido
         self.fecnac = fecnac
         self.email = email
         self.contraseña = contraseña
-        self.cantreservas = 0 ##aca llevo registro de cuantas compras lleva e cliente. Es relevante tener este dato?
+        self.cantreservas = 0
+
         
     def __str__(self):
-        return(f"""{self.nombre} {self.apellido} de dni {self.dni}, nació en la fecha {self.fecnac}, 
-        su email es {self.email} y realizó {self.cantreservas} reservas""")
+        return(f"""{self.nombre} {self.apellido} de dni {self.dni}, nació en la fecha {self.fecnac}, su email es {self.email}, su contraseña es {self.contraseña}, y realizó {self.cantreservas} reservas""")
+    
+    
+    def contador_reservas(self):
+        self.cantreservas += 1
+
+    
+    def cambiar_dato(self):  
+        print("1.DNI, 2.Nombre, 3.Apellido, 4.Fecha de Nacimiento, 5.Email, 6.Contraseña")
+        opcion = input("Elija el número que se encuentra junto al dato que desea modificar: ")
+        dato_nuevo = input("Ingrese su nuevo dato: ")
+        dato_viejo = None
+
+        if opcion == "1":
+            dato_viejo = self.dni
+            self.dni = dato_nuevo
+            print("Su DNI ha sido modificado exitosamente de: ", dato_viejo, " a: ", dato_nuevo)
+            
+        elif opcion == "2":
+            dato_viejo = self.nombre
+            self.nombre = dato_nuevo
+            print("Su Nombre ha sido modificado exitosamente de: ", dato_viejo, " a: ", dato_nuevo)
+
+        elif opcion == "3":
+            dato_viejo = self.apellido
+            self.apellido = dato_nuevo
+            print("Su Apellido ha sido modificado exitosamente de: ", dato_viejo, " a: ", dato_nuevo) 
+
+        elif opcion == "4":
+            dato_viejo = self.fecnac
+            self.fecnac = dato_nuevo
+            print("Su fecha de nacimiento ha sido modificado exitosamente de: ", dato_viejo, " a: ", dato_nuevo)
+
+        elif opcion == "5":
+            dato_viejo = self.email
+            self.email = dato_nuevo
+            print("Su email ha sido modificado exitosamente de: ", dato_viejo, " a: ", dato_nuevo)
+
+        elif opcion == "6":
+            dato_viejo = self.dni
+            self.contraseña = dato_nuevo
+            print("Su contraseña ha sido modificada exitosamente de: ", dato_viejo, " a: ", dato_nuevo)
+
+
+# Pruebas de Funcionamiento
+
+# yo = Personas(44788302, "Juan Cruz", "Varela", "28-04-2023", "jvarela@itba.eduar", "juancin")
+
+# print(yo)
+# yo.cambiar_dato()
 

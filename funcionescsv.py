c1855cde4148f16db3ed12d800be6e2d97218708
juancamu19 <jcamuyrano@itba.edu.ar>
2023-06-06 17:38:15 -0300

    arreglo de funcion asignar auto

--- funcionescsv.py
+++ funcionescsv.py
@@ -23,14 +23,15 @@ def leerCsv(archivo, clase):
     return dic1
    
 
-def asignarauto(fecinicio, fecfin,tipo,gama,diccreservas,diccVehiculos):
+def asignarauto(fecinicio, fecfin, tipo, gama, diccreservas, diccVehiculos):
     
     setvehiculosdisponibles=Vehiculos.setVehiculos
     for k in diccreservas.keys():
         if diccVehiculos[diccreservas[k].patente_auto].tipo==tipo and diccVehiculos[diccreservas[k].patente_auto].gama==gama:
             if (fecinicio >= diccreservas[k].fechaInicio and fecinicio <= diccreservas[k].fechaFin) or ( fecfin <= diccreservas[k].fechaFin and fecfin >= diccreservas[k].fechaIncio) or (fecinicio<=diccreservas[k].fechaInicio and fecfin >= diccreservas[k].fechaFin):
-            
-                setvehiculosdisponibles.difference_update(diccreservas[k].patente_auto)  ##NO ME UPDATEA EL SET
+                
+                setvehiculosdisponibles.remove(diccreservas[k].patente_auto)  ##NO ME UPDATEA EL SET
+
     if len(setvehiculosdisponibles)==0:
         return None
     else:
@@ -40,15 +41,15 @@ def asignarauto(fecinicio, fecfin,tipo,gama,diccreservas,diccVehiculos):
 # Pruebas de Funcionamiento
 if __name__ == "__main__":
     diccr=leerCsv('Reservas.csv',Reserva)
-    # diccv=leerCsv('Vehiculos.csv',Vehiculos)
-    # for key in diccr.keys():
-    #     diccr[key].fechaInicio = datetime.strptime(diccr[key].fechaInicio,"%d-%m-%Y").date()
-    #     diccr[key].fechaFin = datetime.strptime(diccr[key].fechaFin,"%d-%m-%Y").date()
-    # fecinicio=datetime.strptime('10-7-2023',"%d-%m-%Y").date()
-    # fecfin=fecfin=datetime.strptime('25-7-2023',"%d-%m-%Y").date()
-    # auto=asignarauto(fecinicio,fecfin,'urbano','baja',diccr,diccv)
-    # print(auto)
-    Reserva.cambiarfechaInicioAlquiler('2',diccr)
-    escribirCsv('Reservas.csv',diccr)
+    diccv=leerCsv('Vehiculos.csv',Vehiculos)
+    for key in diccr.keys():
+        diccr[key].fechaInicio = datetime.strptime(diccr[key].fechaInicio,"%d-%m-%Y").date()
+        diccr[key].fechaFin = datetime.strptime(diccr[key].fechaFin,"%d-%m-%Y").date()
+    fecinicio=datetime.strptime('10-7-2023',"%d-%m-%Y").date()
+    fecfin=fecfin=datetime.strptime('25-7-2023',"%d-%m-%Y").date()
+    auto=asignarauto(fecinicio,fecfin,'urbano','baja',diccr,diccv)
+    print(auto)
+    # Reserva.cambiarfechaInicioAlquiler('2',diccr)
+    # escribirCsv('Reservas.csv',diccr)
 
     

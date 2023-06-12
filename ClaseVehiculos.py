78b8ffd366266aef94e59adb5245f6660f4424e7
SantiMujik <smujicabuj@itba.edu.ar>
2023-06-12 03:52:42 -0300

    pruebas en vehiculos y cambio en modificar

--- ClaseVehiculos.py
+++ ClaseVehiculos.py
@@ -25,7 +25,24 @@ class Vehiculos():
             case 'patente':
                 patentenueva=input('ingrese patente nueva')
                 self.patente=patentenueva
-                #####Y ASI CON EL RESTO DE ATRIBUTOS #################################  
+                #####Y ASI CON EL RESTO DE ATRIBUTOS ################################# 
+            case 'modelo' :
+                modelonuevo=input('ingrese modelo nuevo')
+                self.modelo=modelonuevo 
+            case 'marca' :
+                marcanueva=input('ingrese marca nueva')
+                self.marca=marcanueva
+            case 'anio':
+                anionuevo=input('ingrese año nuevo')
+                self.anio=anionuevo
+                #validamos que el año sea menor que el año actual? 
+            case 'tipo' :
+                tiponuevo=input ('ingrese nuevo tipo')
+                self.tipo=tiponuevo
+            case 'gama':
+                gamanueva=input('ingrese nueva gama')
+                self.gama=gamanueva
+            ##No se como se haria esto con el tema del precio xq esta el asignar precio, faltaria eso 
     def eliminar(self,diccVehi):
         del diccVehi[self.patente]
         
@@ -41,13 +58,17 @@ class Vehiculos():
             obj_list.append(value)
         return obj_list
         
-         
+
 
     def __str__(self):     #Sería como una consulta general sobre el estado de un vehículo
         return(f"""El vehículo {self.marca} {self.modelo} de patente {self.patente}, año {self.anio}, tiene un precio de alquiler por día de {self.precio}, ...""") 
     
 # Pruebas de Funcionamiento
+#creo un falcon de pruebas
+TESTER=Vehiculos('abc500','Falcon','Ford',2023,'deportivo','alta',60000,True)
+
 if __name__ == "__main__":
-   print(Vehiculos.asignar_precio('deportivo','alta'))
-    
+   print(Vehiculos.asignarPrecio(TESTER))
+   print(Vehiculos.objeto_a_lista(TESTER))
+   print(Vehiculos.modificar(TESTER,'patente'))
 
